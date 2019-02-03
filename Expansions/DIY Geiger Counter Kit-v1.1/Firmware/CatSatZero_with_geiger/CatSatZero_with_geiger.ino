091663a633f0b1c1e03668b7a5bd5e17a053217f
sabas1080 <s@theinventorhouse.org>
2019-02-03 22:30:30 +0100

    Fix remove extra value

--- Expansions/DIY Geiger Counter Kit-v1.1/Firmware/CatSatZero_with_geiger/CatSatZero_with_geiger.ino
+++ Expansions/DIY Geiger Counter Kit-v1.1/Firmware/CatSatZero_with_geiger/CatSatZero_with_geiger.ino
@@ -407,9 +407,7 @@ void printInfoSerial()
   SerialUSB.print(" Pressure: ");
   SerialUSB.print((myBME280.readFloatPressure() * 0.0002953), 2);
   SerialUSB.println(" InHg");
-  #endif
-  Todo += myCCS811.getCO2();
-  Todo += ","; 
+  #endif 
 
   #ifdef DEBUG
   SerialUSB.print(" Altitude: ");
